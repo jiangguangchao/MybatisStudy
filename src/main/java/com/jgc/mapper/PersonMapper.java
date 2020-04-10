1d261c74f9686e4a973bb7af892bb6d182494adb
jgc <jiangguangchao157@163.com>
2020-04-10 15:16:06 +0800

    添加影射文件对应的接口，并使用单元测试，测试查询功能

---
+++ src/main/java/com/jgc/mapper/PersonMapper.java
@@ -0,0 +1,8 @@
+package com.jgc.mapper;
+
+import com.jgc.bean.Person;
+
+public interface PersonMapper {
+
+    public Person getPersonByID(Integer id);
+}
---
+++ src/test/java/com/jgc/MyTest.java
@@ -0,0 +1,37 @@
+package com.jgc;
+
+import com.jgc.bean.Person;
+import com.jgc.mapper.PersonMapper;
+import com.jgc.mysql.SqlSessionMag;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @description:
+ * @author:jgc
+ * @create:2020-04-10 15:00
+ */
+public class MyTest {
+
+    public static final Logger log = LoggerFactory.getLogger(MyTest.class);
+
+    @Test
+    public void query() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = new SqlSessionMag().getSqlSession();
+            Class personMapperClass= Class.forName("com.jgc.mapper.PersonMapper");
+            PersonMapper personMapper = (PersonMapper) sqlSession.getMapper(personMapperClass);
+            Person person = personMapper.getPersonByID(2);
+            log.info(person.toString());
+        }catch (Exception e) {
+            log.error("异常信息", e);
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+}
